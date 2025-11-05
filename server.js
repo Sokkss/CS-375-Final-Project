1687550dd1e053d991fd9bd724636c38ea157d88
Sokkss <124085882+Sokkss@users.noreply.github.com>
2025-11-04 23:02:43 -0500

    updating structure a little more and readme

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-let express = require("express");
+let app = require("./src/app");
 
 let hostname = "localhost";
 let port = 3000;
-let app = express();
-
-app.use(express.json());
-app.use(express.static("public"));
 
 app.listen(port, hostname, () => {
     console.log(`Listening at: http://${hostname}:${port}`);
