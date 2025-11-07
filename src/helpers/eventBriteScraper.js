7135b44004df622820a9fb133a4e9862a05b698c
ishakothari1 <idk23@drexel.edu>
2025-11-07 12:49:06 -0500

    add initial objects

--- src/helpers/eventBriteScraper.js
+++ src/helpers/eventBriteScraper.js
@@ -7,38 +7,19 @@ const cheerio = require('cheerio');
 
     const $ = cheerio.load(await response.text());
     
-    const RESULTS_SELECTOR = '.search-results-panel-content_events';
-    const ITEM_SELECTOR = 'ul';
-
+    const RESULTS_SELECTOR = '.SearchResultPanelContentEventCardList-module__eventList___2wk-D';
+   
     const events = $(RESULTS_SELECTOR).map((i, event) => {
         const $eventSection = $(event);
 
-        const title = $eventSection.find('a').attr('aria-label').text().trim();
+        let $body = $eventSection.find('.event-card-details');
 
-        let $body = $eventSection.find('.vp-article-section__body').clone();
-        $body.find('.vp-body-subhead-2').remove();
+        const title = $body.find('a').attr('aria-label');
+        const date = $body.find('p').text().trim();
+        const description = $body.find('a').attr('href');
+        const location = $body.find('p').text().trim();
 
-        const description = $body.text()
-            .replace(/\s+/g, ' ')
-            .replaceAll('\n', ' ')
-            .replaceAll('\t', ' ')
-            .trim();
-    
-        const date = $eventSection.find('.vp-article-section__date-time').text()
-            .replace(/\s\|.*/, '')
-            .trim();
-
-        const location = $eventSection.find('.vp-article-section__details').text()
-            .replace(/\t/g, "")
-            .split('\n')
-            .filter(item => item !== "")
-            .filter(str => str.trim() !== "")
-
-        if (date.length === 0 || locations.length === 0 || descriptions.length === 0) {
-            return null;
-        }
-
-        return {title, date, location, description};
+        return {title, date, description, location};
     }).get().flat();
 
     let formattedEvents = []
@@ -46,12 +27,8 @@ const cheerio = require('cheerio');
     for (let section of events) {
         let title = section.title;
         let date = section.date;
-        let locations = section.locations;
+        let location = section.location;
         let description = section.description;
-
-        if (items.length !== locations.length) {
-            continue;
-        }
 
         let event = {title, date, location, description};
         formattedEvents.push(event);
