0aa3ac4be35997e21c97bab8e6c9d1cdd5b2d04e
madelb <153689098+madelb@users.noreply.github.com>
2025-11-12 15:48:21 -0500

    fixes for new location format

--- src/services/seatgeek.service.js
+++ src/services/seatgeek.service.js
@@ -8,7 +8,6 @@ async function fetchPhillyEventsNextWeek() {
 
     const params = {
         client_id: process.env.SEATGEEK_CLIENT_ID,
-        //client_secret: process.env.SEATGEEK_CLIENT_SECRET,
         lat: 39.9526,
         lon: -75.1652,  // this is roughly city hall coords
         range: '25mi',
@@ -22,18 +21,30 @@ async function fetchPhillyEventsNextWeek() {
 
     const mappedEvents = res.data.events.map((item, index) => {
         const id = `seatgeek-event-${index}`;
-        const title = item.title;
-        const description = item.url;
-        const location = item.venue.location;
+        const title = item.short_title;
+        const description = `${item.title} at ${item.venue.name}. More information here: ${item.url}`;
+        const locationDescription = `${item.venue.address} ${item.venue.city} ${item.venue.state} ${item.venue.postal_code}`;
+        const lat = item.venue.location.lat;
+        const long = item.venue.location.lon;
         const time = item.datetime_utc;
         const owner = 'Seatgeek';
         const image = null;
         const externalLink = item.url;
 
-        return new Event(id, title, description, location, time, owner, image, externalLink);
+        return new Event(id, title, description, locationDescription, lat, long, time, owner, image, externalLink);
     });
 
     return mappedEvents;
 }
+
+// uncomment below for testing
+
+// (async () => {
+//     try {
+//         const events = await fetchPhillyEventsNextWeek();
+//     } catch (err) {
+//         console.error("Error fetching events:", err.message);
+//     }
+// })();
 
 module.exports = fetchPhillyEventsNextWeek;
