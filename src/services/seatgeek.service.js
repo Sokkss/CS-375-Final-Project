4d6a7cd546ddcc3f9876673e5740c729d452563e
madelb <153689098+madelb@users.noreply.github.com>
2025-12-05 00:20:44 -0500

    no more eventbrite rip

--- src/services/seatgeek.service.js
+++ src/services/seatgeek.service.js
@@ -6,35 +6,46 @@ async function fetchPhillyEventsNextWeek() {
     const now = new Date();
     const endDateTime = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
 
-    const params = {
-        client_id: process.env.SEATGEEK_CLIENT_ID,
-        lat: 39.9526,
-        lon: -75.1652,  // this is roughly city hall coords
-        range: '25mi',
-        'datetime_utc.gte': startDateTime,
-        'datetime_utc.lte': endDateTime,
-        per_page: 50,
-        page: 1,
-    };
+    const allEvents = [];
+    const numPages = 5;
 
-    const res = await axios.get('https://api.seatgeek.com/2/events', { params });
+    // Fetch multiple pages
+    for (let page = 1; page <= numPages; page++) {
+        const params = {
+            client_id: process.env.SEATGEEK_CLIENT_ID,
+            lat: 39.9526,
+            lon: -75.1652,  // this is roughly city hall coords
+            range: '25mi',
+            'datetime_utc.gte': startDateTime,
+            'datetime_utc.lte': endDateTime,
+            per_page: 50,
+            page: page,
+        };
+        const res = await axios.get('https://api.seatgeek.com/2/events', { params });
+        
+        if (!res.data.events || res.data.events.length === 0) {
+            break;
+        }
 
-    const mappedEvents = res.data.events.map((item, index) => {
-        const id = `seatgeek-event-${index}`;
-        const title = item.short_title;
-        const description = `${item.title} at ${item.venue.name}. More information here: ${item.url}`;
-        const locationDescription = `${item.venue.address} ${item.venue.city} ${item.venue.state} ${item.venue.postal_code}`;
-        const lat = item.venue.location.lat;
-        const long = item.venue.location.lon;
-        const time = item.datetime_utc;
-        const owner = 'Seatgeek';
-        const image = null;
-        const externalLink = item.url;
+        const mappedEvents = res.data.events.map((item, index) => {
+            const id = `seatgeek-event-${page}-${index}`;
+            const title = item.short_title;
+            const description = `${item.title} at ${item.venue.name}. More information here: ${item.url}`;
+            const locationDescription = `${item.venue.address} ${item.venue.city} ${item.venue.state} ${item.venue.postal_code}`;
+            const lat = item.venue.location.lat;
+            const long = item.venue.location.lon;
+            const time = item.datetime_utc;
+            const owner = 'Seatgeek';
+            const image = null;
+            const externalLink = item.url;
 
-        return new Event(id, title, description, locationDescription, lat, long, time, owner, image, externalLink);
-    });
+            return new Event(id, title, description, locationDescription, lat, long, time, owner, image, externalLink);
+        });
 
-    return mappedEvents;
+        allEvents.push(...mappedEvents);
+    }
+
+    return allEvents;
 }
 
 // uncomment below for testing
